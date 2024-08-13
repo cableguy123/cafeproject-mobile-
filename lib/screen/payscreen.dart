6740baeb7ae9241dcb46263a4a21eb27268499c2
Master1 <cableguy123@naver.com>
2024-08-14 03:55:39 +0900

    screennavi

--- lib/screen/payscreen.dart
+++ lib/screen/payscreen.dart
@@ -1,3 +1,4 @@
+import 'package:cafeproject/screen/homescreen.dart';
 import 'package:flutter/material.dart';
 
 
@@ -5,8 +6,21 @@ class PayScreen extends StatelessWidget {
   const PayScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello Wolrd!',
-    );
+    return const PayScreenBodyWidget();
   }
 }
+
+class PayScreenBodyWidget extends StatefulWidget {
+  const PayScreenBodyWidget({super.key});
+  @override
+  State<PayScreenBodyWidget> createState() => _PayScreenBodyWidgetState();
+}
+class _PayScreenBodyWidgetState extends State<PayScreenBodyWidget> with AutomaticKeepAliveClientMixin<PayScreenBodyWidget>{
+  @override
+  bool get wantKeepAlive => true;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
