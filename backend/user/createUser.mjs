334987a0824eb744a3d990174d1eda92a5cfa612
Master1 <cableguy123@naver.com>
2024-09-23 13:03:21 +0900

    password salt createUser

--- backend/user/createUser.mjs
+++ backend/user/createUser.mjs
@@ -1,10 +1,8 @@
 import sqlite3 from 'sqlite3';
 import crypto from 'crypto';
 import util from 'util';
-import { selectUser } from './signup.mjs';
 
-const db = new sqlite3.Database('/Users/imseoghyeon/testDb1');
-
+const db = new sqlite3.Database('/Users/imseoghyeon/nodeDB');
 const randomBytesPromise = util.promisify(crypto.randomBytes);
 const pbkdf2Promis = util.promisify(crypto.pbkdf2);
 
@@ -12,28 +10,34 @@ const createSalt = async () => {
   const buf = await randomBytesPromise(64);
   return buf.toString("base64");
 }
+
 const createHashPassword = async (password) => {
   const salt = await createSalt();
-  const key = await pbkdf2Promis(password,salt, 104906, 64, "sha512");
+  const key = await pbkdf2Promis(password, salt, 104906, 64, "sha512");
   const hashedPassword = key.toString("base64");
-  return { hashedPassword,salt };
+  return { hashedPassword, salt };
 }
+
 export function createUser(req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
-  createHashPassword(password).then(({hashedPassword}) => {
-    db.run('INSERT INTO USER (email, password) VALUES (?, ?)', [email, hashedPassword], function (err) {
-      if (err) {
-        return res.status(400).json({
-          error: "登録済みのメール",
-        });
-      } else {
-        return res.status(200).json({
-          email: email,
-          password: hashedPassword
-        });
+  const { name, email, password, fomrSalt ,created_at} = req.body;
+  const createdAt = new Date().toISOString().split('T')[0];
+  createHashPassword(password).then(({ hashedPassword, salt }) => {
+    db.run('INSERT INTO users (name, email, password, salt, created_at) VALUES (?, ?, ?, ?, ?)', [name, email, hashedPassword, salt, createdAt],function (err) {
+        if (err) {
+          return res.status(400).json({
+            error: "登録済みのメール",
+          });
+        } else {
+          return res.status(200).json({
+            name: name,
+            email: email,
+            password: hashedPassword,
+            fomrSalt: salt,
+            created_at: createdAt
+          });
+        }
       }
-    });
+    );
   }).catch(error => {
     return res.status(500).json({
       error: "例外",
