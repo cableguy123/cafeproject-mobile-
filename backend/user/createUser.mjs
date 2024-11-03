153a6d2f027178e1ad6a3236c864bf4edc69e6e3
sawauchi ryota <ryota0922gks@gmail.com>
2024-11-03 22:29:02 +0900

    temporary delete function

--- backend/user/createUser.mjs
+++ backend/user/createUser.mjs
@@ -2,7 +2,8 @@ import sqlite3 from 'sqlite3';
 import crypto from 'crypto';
 import util from 'util';
 
-const db = new sqlite3.Database('/Users/imseoghyeon/nodeDB');
+const db = new sqlite3.Database('/Users/sawauchiryouta/cafe-project-mobile-db/cafe-project-mobile-db.db');
+
 const randomBytesPromise = util.promisify(crypto.randomBytes);
 const pbkdf2Promis = util.promisify(crypto.pbkdf2);
 
@@ -19,24 +20,24 @@ const createHashPassword = async (password) => {
 }
 
 export function createUser(req, res) {
-  const { name, email, password, fomrSalt ,created_at} = req.body;
+  const { name, email, password, fomrSalt, created_at } = req.body;
   const createdAt = new Date().toISOString().split('T')[0];
   createHashPassword(password).then(({ hashedPassword, salt }) => {
-    db.run('INSERT INTO users (name, email, password, salt, created_at) VALUES (?, ?, ?, ?, ?)', [name, email, hashedPassword, salt, createdAt],function (err) {
-        if (err) {
-          return res.status(400).json({
-            error: "登録済みのメール",
-          });
-        } else {
-          return res.status(200).json({
-            name: name,
-            email: email,
-            password: hashedPassword,
-            fomrSalt: salt,
-            created_at: createdAt
-          });
-        }
+    db.run('INSERT INTO users (name, email, password, created_at) VALUES (?, ?, ?, ?)', [name, email, hashedPassword, createdAt], function (err) {
+      if (err) {
+        return res.status(400).json({
+          error: "登録済みのメール",
+        });
+      } else {
+        return res.status(200).json({
+          name: name,
+          email: email,
+          password: hashedPassword,
+          fomrSalt: salt,
+          created_at: createdAt
+        });
       }
+    }
     );
   }).catch(error => {
     return res.status(500).json({
--- backend/user/signup.mjs
+++ backend/user/signup.mjs
@@ -1,11 +1,12 @@
 import sqlite3 from 'sqlite3';
 
-const db = new sqlite3.Database('/Users/imseoghyeon/testDB');
+const db = new sqlite3.Database('/Users/sawauchiryouta/cafe-project-mobile-db/cafe-project-mobile-db.db');
 
-export function selectUser(req,res) {
+
+export function selectUser(req, res) {
     const { email, password } = req.body;
-    db.run('SELECT * FROM USER WHERE email = ? AND password = ?',[email,password], (err) => {
-        if(err) {
+    db.run('SELECT * FROM USERS WHERE email = ? AND password = ?', [email, password], (err) => {
+        if (err) {
             console.error(err.message);
             res.status(500).send('Error SELECT USER');
         } else {
